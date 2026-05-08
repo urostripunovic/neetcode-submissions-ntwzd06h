9b004af1f37e3b18184e36923af47257dd77690c
urostripunovic <39649964+urostripunovic@users.noreply.github.com>
2026-05-09 00:20:33 +0200

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.go
@@ -0,0 +1,68 @@
+func isValid(s string) bool {
+	if len(s) % 2 != 0 {
+		return false
+	}
+
+	stack := Stack[rune]{
+		items: make([]rune, 0, len(s)),
+	}
+
+    for _, ch := range s {
+		switch ch {
+			case ')':
+				if stack.Empty() { return false } 
+				if top, ok := stack.Peek(); top != '(' && ok {
+					return false
+				}
+				stack.Pop()
+			case ']':
+				if stack.Empty() { return false } 
+				if top, ok := stack.Peek(); top != '[' && ok {
+					return false
+				}
+				stack.Pop()
+			case '}':
+				if stack.Empty() { return false } 
+				if top, ok := stack.Peek(); top != '{' && ok {
+					return false
+				}
+				stack.Pop()
+			default:
+				stack.Push(ch)
+		}
+	}
+	return stack.Empty()
+}
+
+type Stack[T any] struct {
+	items []T
+}
+
+func (this *Stack[T]) Push(val T) {
+	this.items = append(this.items, val)
+}
+
+func (this *Stack[T]) Pop() (T, bool) {
+	if len(this.items) == 0 {
+		var zero T
+		return zero, false
+	}
+	topIndex := len(this.items) - 1
+	val := this.items[topIndex]
+	this.items = this.items[:topIndex]
+	return val, true
+}
+
+func (this *Stack[T]) Peek() (T, bool) {
+	if len(this.items) == 0 {
+		var zero T
+		return zero, false
+	}
+	topIndex := len(this.items) - 1
+	val := this.items[topIndex]
+	return val, true
+}
+
+func (this *Stack[T]) Empty() bool {
+	return len(this.items) == 0
+}
